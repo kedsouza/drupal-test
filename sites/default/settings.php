d14d4a016446049e8c5c136ac5bb4d5ec72467a9
kedsouza <55401636+kedsouza@users.noreply.github.com>
2023-09-15 11:14:03 -0500

    Update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -494,7 +494,7 @@ $settings['update_free_access'] = FALSE;
  * must exist and be writable by Drupal. This directory must be relative to
  * the Drupal installation directory and be accessible over the web.
  */
-# $settings['file_assets_path'] = 'sites/default/files';
+ $settings['file_assets_path'] = '/fileshare/sites/default/files';
 
 /**
  * Public file base URL:
